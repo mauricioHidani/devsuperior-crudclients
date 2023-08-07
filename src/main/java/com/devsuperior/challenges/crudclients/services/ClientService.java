e534657dbada7070663da3e4dd93638985382725
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-08-07 14:25:51 -0300

    feat: client find by id

---
+++ src/main/java/com/devsuperior/challenges/crudclients/services/ClientService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.challenges.crudclients.services;
+
+import com.devsuperior.challenges.crudclients.dtos.ClientDTO;
+import com.devsuperior.challenges.crudclients.entities.Client;
+import com.devsuperior.challenges.crudclients.repositories.ClientRepository;
+import com.devsuperior.challenges.crudclients.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired private ClientRepository clientRepository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client result = clientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente inexistente"));
+        return new ClientDTO(result);
+    }
+}
