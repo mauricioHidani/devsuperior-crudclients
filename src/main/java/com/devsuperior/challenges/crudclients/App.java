5b036363f7b4454a59c9551cd10eecf9566dc66b
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2023-08-07 14:11:52 -0300

    init: challenge crud clients

---
+++ src/main/java/com/devsuperior/challenges/crudclients/App.java
@@ -0,0 +1,10 @@
+package com.devsuperior.challenges.crudclients;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
